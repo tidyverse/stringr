d198d6f059ebc032a96d7c9b50f6a3689931f490
hadley <h.wickham@gmail.com>
2009-11-08 15:08:14 -0800

    Improve extract docs

--- R/regexp.r
+++ R/regexp.r
@@ -75,6 +75,7 @@ str_locate_all <- function(string, pattern) {
 #' @param pattern pattern to look for.  See \code{\link{regex}} for
 #'   description.
 #' @return character vector.
+#' @seealso \code{\link{str_extract_all}} to extract all matches
 #' @examples
 #' shopping_list <- c("apples x4", "flour", "sugar", "milk x2")
 #' str_extract(shopping_list, "\\d")
@@ -94,7 +95,10 @@ str_extract <- function(string, pattern) {
 #'   description.
 #' @return list of character vectors.
 #' @examples
-#' shopping_list <- c("apples x4", "flour", "sugar", "milk x2")
+#' @seealso \code{\link{str_extract}} to extract the first match
+#' shopping_list <- c("apples x4", "bag of flour", "bag of sugar", "milk x2")
+#' str_extract_all(shopping_list, "[a-z]+")
+#' str_extract_all(shopping_list, "\\b[a-z]+\\b")
 #' str_extract_all(shopping_list, "\\d")
 str_extract_all <- function(string, pattern) {
   positions <- str_locate_all(string, pattern)
