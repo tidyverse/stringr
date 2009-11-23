289e97cb4b067262f91767f9208a3bb6bb219eba
hadley <h.wickham@gmail.com>
2009-11-23 08:31:56 -0600

    Add function to convert regular expression to fixed string

---
+++ R/fixed.r
@@ -0,0 +1,16 @@
+#' Escape all special regular expression characters.
+#' 
+#' This function escapes all characters that have meaning for regular
+#' expressions so the string will be matched exactly as is.
+#' 
+#' @param string string to match exactly as is
+#' @author William Dunlap \email{wdunlap@@tibco.com}
+#' @keywords character
+#' @examples
+#' pattern <- "a.b"
+#' strings <- c("abb", "a.b")
+#' str_detect(strings, pattern)
+#' str_detect(strings, fixed(pattern))
+fixed <- function(string) {
+   str_replace(string, "([][^${}().?*+\\])", "\\\\\\1")
+}
