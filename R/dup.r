3b0a5816bb9aa4767454771d92a428dd651ad764
hadley <h.wickham@gmail.com>
2009-12-01 08:57:23 -0600

    Improve str_dup documentaiton

--- R/dup.r
+++ R/dup.r
@@ -1,15 +1,19 @@
-#' Duplicate strings within a character vector.
+#' Duplicate and concatenate strings within a character vector.
 #'
 #' Vectorised over \code{string} and \code{times}.
 #'
 #' @param string input character vector
 #' @param times number of times to duplicate each string
 #' @return character vector
-#' @keywords internal
+#' @keywords character
+#' @examples
+#' fruit <- c("apple", "pear", "banana")
+#' str_dup(fruit, 2)
+#' str_dup(fruit, 1:3)
+#' str_c("ba", str_dup("na", 0:5))
 str_dup <- function(string, times) {
   string <- check_string(string)
 
-  # rep_matrix <- matrix(rep(string, times = times), nrow = times)
   strings <- mlply(cbind(x = string, times), rep.int)
   output <- unlist(llply(strings, str_c, collapse = ""))
 
