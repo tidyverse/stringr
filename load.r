dfb77bddd89fd19e423e718d24f3cd9b84052913
hadley <h.wickham@gmail.com>
2010-08-24 08:10:43 -0500

    Remove old load code

--- load.r
+++
@@ -1,6 +0,0 @@
-library(plyr, warn.conflicts = FALSE)
-
-frame_files <- compact(llply(sys.frames(), function(x) x$ofile))
-PATH <- dirname(frame_files[[length(frame_files)]])
-
-lapply(dir(file.path(PATH, "R"), full.name=TRUE), source)
