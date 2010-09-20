03bb631ad88f08161b6e611249018e1036c16841
hadley <h.wickham@gmail.com>
2010-09-20 08:46:23 -0500

    A few more str_length tests inspired by paper

--- inst/tests/test-length.r
+++ inst/tests/test-length.r
@@ -1,10 +1,19 @@
 context("String length")
 
-test_that("length of missing string is missing", {
+test_that("str_length is number of characters", {
+  expect_that(str_length("a"), equals(1))
+  expect_that(str_length("ab"), equals(2))
+  expect_that(str_length("abc"), equals(3))
+})
+
+test_that("str_length of missing string is missing", {
   expect_that(str_length(NA), equals(NA_integer_))
+  expect_that(str_length(c(NA, 1)), equals(c(NA, 1)))
   expect_that(str_length("NA"), equals(2))
 })
 
-test_that("works with factors", {
+test_that("str_length of factor is length of level", {
+  expect_that(str_length(factor("a")), equals(1))
+  expect_that(str_length(factor("ab")), equals(2))
   expect_that(str_length(factor("abc")), equals(3))
 })
