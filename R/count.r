abef74bd11a61e4b9a342438dbdb5561137bdd66
hadley <h.wickham@gmail.com>
2011-04-18 09:52:36 -0500

    str_count counts the number of matches

---
+++ R/count.r
@@ -0,0 +1,43 @@
+#' Count the number of matches in a string.
+#'
+#' Vectorised over \code{string} and \code{pattern}, shorter is recycled to
+#' same length as longest.
+#'
+#' @param string input character vector
+#' @param pattern pattern to look for, as defined by a POSIX regular
+#'   expression.  See the ``Extended Regular Expressions'' section of 
+#'   \code{\link{regex}} for details.
+#' @keywords character
+#' @return integer vector
+#' @seealso
+#'  \code{\link{regexpr}} which this function wraps
+#'
+#'  \code{\link{str_locate}}/\code{\link{str_locate_all}} to locate position
+#'  of matches
+#' 
+#' @export
+#' @examples
+#' fruit <- c("apple", "banana", "pear", "pineapple")
+#' str_count(fruit, "a")
+#' str_count(fruit, "p")
+#' str_count(fruit, "e")
+#' str_count(fruit, c("a", "b", "p", "p"))
+str_count <- function(string, pattern) {
+  if (length(string) == 0) return(character())
+  string <- check_string(string)
+  pattern <- check_pattern(pattern, string)
+
+  if (length(pattern) == 1) {
+    matches <- re_call("gregexpr", string, pattern)
+  } else {
+    matches <- unlist(re_mapply("gregexpr", string, pattern), 
+      recursive = FALSE)
+  }
+
+  match_length <- function(x) {
+    len <- length(x)
+    if (len > 1) return(len)
+    if (identical(c(x), -1L)) 0L else 1L
+  }
+  vapply(matches, match_length, integer(1))
+}
---
+++ inst/tests/test-count.r
@@ -0,0 +1,11 @@
+context("Counting matches")
+
+test_that("counts are as expected", {
+  fruit <- c("apple", "banana", "pear", "pineapple")
+  expect_equal(str_count(fruit, "a"), c(1, 3, 1, 1))
+  expect_equal(str_count(fruit, "p"), c(2, 0, 1, 3))
+  expect_equal(str_count(fruit, "e"), c(1, 0, 1, 2))
+  expect_equal(str_count(fruit, c("a", "b", "p", "n")), c(1, 1, 1, 1))
+  
+  
+})
