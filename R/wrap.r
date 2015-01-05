bd4e71fb362f5cb6d2bc44b183708a83f2b99217
hadley <h.wickham@gmail.com>
2015-01-05 15:21:12 -0600

    If width <= 0, use width = 1

--- R/wrap.r
+++ R/wrap.r
@@ -4,7 +4,8 @@
 #' the Knuth-Plass paragraph wrapping algorithm.
 #'
 #' @param string character vector of strings to reformat.
-#' @param width positive integer giving target line width in characters.
+#' @param width positive integer giving target line width in characters. A
+#'   width less than or equal to 1 will put each word on its own line.
 #' @param indent non-negative integer giving indentation of first line in
 #'  each paragraph
 #' @param exdent non-negative integer giving indentation of following lines in
@@ -19,8 +20,9 @@
 #' cat(str_wrap(thanks, width = 40), "\n")
 #' cat(str_wrap(thanks, width = 60, indent = 2), "\n")
 #' cat(str_wrap(thanks, width = 60, exdent = 2), "\n")
+#' cat(str_wrap(thanks, width = 0, exdent = 2), "\n")
 str_wrap <- function(string, width = 80, indent = 0, exdent = 0) {
-  if (width == 0) return(string)
+  if (width <= 0) width <- 1
 
   out <- stri_wrap(string, width = width, indent = indent, exdent = exdent,
     simplify = FALSE)
