1a84824d6ecdcd1d87247c2a7046e9d252c348fa
Gerhard Nachtmann <gerhard.nachtmann@statistik.gv.at>
2015-01-20 15:40:31 +0100

    renamed due to stringr conventions

--- R/left.R
+++ R/left.R
@@ -1,30 +1,31 @@
 #' 
 #' Left- or right-most element(s)
 #' 
-#' Extract the \code{n} left-most (\code{left}) or right-most
-#' (\code{right}) elements of \code{x}.
+#' Extract the \code{n} left-most (\code{str_left}) or right-most
+#' (\code{str_right}) elements of \code{x}.
 #' 
-#' @aliases left right
+#' @aliases str_left str_right
 #' @param x a vector that you wish to extract from.
 #' @param n an optional integer indicating the number of
 #' elements you wish to extract from \code{x} starting from the
 #' left-most (\code{left}) or right-most (\code{right}) element.
 #' @return A character vector containing the \code{n} left-most
-#' (\code{left}) or right-most (\code{right}) elements of
-#' \code{x}.
+#' (\code{str_left}) or right-most (\code{str_right}) elements
+#' of \code{x}.
 #' @author Gerhard Nachtmann \email{kpm.nachtmann@@gmail.com}
-#' @seealso \code{\link{substr}}, \code{\link{nchar}}
+#' @seealso \code{\link{substr}}, \code{\link{nchar}},
+#' \code{\link{str_sub}}
 #' @keywords left right
 #' @export
 #' @examples
 #'
 #'   x <- c("abcde", "12345")
-#'   left(x)
-#'   left(x, n = 3)
-#'   right(x)
-#'   right(x, n = 2)
+#'   str_left(x)
+#'   str_left(x, n = 3)
+#'   str_right(x)
+#'   str_right(x, n = 2)
 
-left <-
+str_left <-
 function(x, n = 1){
     x <- as.character(x)
     begin <- 1
@@ -34,9 +35,9 @@ function(x, n = 1){
 
 
 #' @export
-#' @rdname left
+#' @rdname str_left
 
-right <-
+str_right <-
 function(x, n = 1){
     x <- as.character(x)
     end <- nchar(x)
