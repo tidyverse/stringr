fb5cfa127ee1d6d4be7d5467b9ef3f664bd0c368
hadley <h.wickham@gmail.com>
2015-10-27 11:02:57 -0500

    Correct arg name

--- R/view.R
+++ R/view.R
@@ -5,18 +5,18 @@
 #' @examples
 #' str_view(c("abc", "def", "fgh"), "[aeiou]")
 #' str_view(c("abc", "def", "fgh"), "^")
-str_view <- function(x, pattern) {
-  loc <- str_locate(x, pattern)
+str_view <- function(string, pattern) {
+  loc <- str_locate(string, pattern)
 
   # How to do escaping? Need to update x and loc
 
   has_match <- !is.na(loc[, "start"])
-  str_sub(x[has_match], loc[has_match, , drop = FALSE]) <-
-    paste0("<span class='match'>", str_sub(x[has_match], loc[has_match, , drop = FALSE]), "</span>")
+  str_sub(string[has_match], loc[has_match, , drop = FALSE]) <-
+    paste0("<span class='match'>", str_sub(string[has_match], loc[has_match, , drop = FALSE]), "</span>")
 
   bullets <- htmltools::HTML(str_c(
     "<ul>\n",
-    str_c("  <li>", x, "</li>", collapse = "\n"),
+    str_c("  <li>", string, "</li>", collapse = "\n"),
     "\n</ul>"
   ))
 
