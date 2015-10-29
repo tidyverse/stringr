4e600bf72229393ea92b663ec32b1121689e7eec
hadley <h.wickham@gmail.com>
2015-10-29 09:12:51 -0500

    Add match argument to str_view

--- R/view.R
+++ R/view.R
@@ -1,11 +1,24 @@
 #' View HTML rendering of regular expression match.
 #'
 #' @inheritParams str_detect
+#' @param match If \code{TRUE}, shows only strings that match the pattern.
+#'   If \code{FALSE}, shows only the strings that don't match the pattern.
+#'   Otherwise (the default) displays both matches and non-matches.
 #' @export
 #' @examples
 #' str_view(c("abc", "def", "fgh"), "[aeiou]")
 #' str_view(c("abc", "def", "fgh"), "^")
-str_view <- function(string, pattern) {
+#'
+#' str_view(c("abc", "def", "fgh"), "d|e", match = TRUE)
+#' str_view(c("abc", "def", "fgh"), "d|e", match = FALSE)
+str_view <- function(string, pattern, match = NA) {
+
+  if (identical(match, TRUE)) {
+    string <- string[str_detect(string, pattern)]
+  } else if (identical(match, FALSE)) {
+    string <- string[!str_detect(string, pattern)]
+  }
+
   loc <- str_locate(string, pattern)
 
   # How to do escaping? Need to update x and loc
