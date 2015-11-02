524692ceeb2e2e2b14f09a5289e323f332b69147
hadley <h.wickham@gmail.com>
2015-11-01 18:32:53 -0600

    Implement str_view_all

--- R/view.R
+++ R/view.R
@@ -1,5 +1,8 @@
 #' View HTML rendering of regular expression match.
 #'
+#' \code{str_view} shows the first match; \code{str_view_all} shows all
+#' the matches.
+#'
 #' @inheritParams str_detect
 #' @param match If \code{TRUE}, shows only strings that match the pattern.
 #'   If \code{FALSE}, shows only the strings that don't match the pattern.
@@ -11,6 +14,8 @@
 #'
 #' str_view(c("abc", "def", "fgh"), "d|e", match = TRUE)
 #' str_view(c("abc", "def", "fgh"), "d|e", match = FALSE)
+#'
+#' str_view_all(c("abc", "def", "fgh"), "d|e")
 str_view <- function(string, pattern, match = NA) {
 
   if (identical(match, TRUE)) {
@@ -26,6 +31,44 @@ str_view <- function(string, pattern, match = NA) {
   has_match <- !is.na(loc[, "start"])
   str_sub(string[has_match], loc[has_match, , drop = FALSE]) <-
     paste0("<span class='match'>", str_sub(string[has_match], loc[has_match, , drop = FALSE]), "</span>")
+
+  bullets <- htmltools::HTML(str_c(
+    "<ul>\n",
+    str_c("  <li>", string, "</li>", collapse = "\n"),
+    "\n</ul>"
+  ))
+
+  htmlwidgets::createWidget("str_view", list(html = bullets),
+    sizingPolicy = htmlwidgets::sizingPolicy(
+      knitr.figure = FALSE,
+      defaultHeight = "auto"
+    ),
+    package = "stringr")
+}
+
+#' @rdname str_view
+#' @export
+str_view_all <- function(string, pattern, match = NA) {
+
+  if (identical(match, TRUE)) {
+    string <- string[str_detect(string, pattern)]
+  } else if (identical(match, FALSE)) {
+    string <- string[!str_detect(string, pattern)]
+  }
+
+  loc <- str_locate_all(string, pattern)
+
+  string_list <- Map(loc = loc, string = string, function(loc, string) {
+    if (nrow(loc) == 0)
+      return(string)
+
+    for (i in rev(seq_len(nrow(loc)))) {
+      str_sub(string, loc[i, , drop = FALSE]) <-
+        paste0("<span class='match'>", str_sub(string, loc[i, , drop = FALSE]), "</span>")
+    }
+    string
+  })
+  string <- unlist(string_list)
 
   bullets <- htmltools::HTML(str_c(
     "<ul>\n",
