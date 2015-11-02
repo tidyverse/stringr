e8d5ef3e30fd8e1df8aecbdc00f040bd583b18eb
hadley <h.wickham@gmail.com>
2015-11-02 10:53:08 -0500

    Tweaking test style

--- tests/testthat/test-replace.r
+++ tests/testthat/test-replace.r
@@ -9,9 +9,9 @@ test_that("replacement strings with capture groups refs and dollar signs work", 
   expect_equal(str_replace_all("abc$a$1$2", fixed("a"), "$1"), "$1bc$$1$1$2")
   expect_equal(str_replace("abc$a$1$2", fixed("a"), "$1"), "$1bc$a$1$2")
 
-  expect_equal(str_replace_all("abcde", "(b)(c)(d)", "\\1"), gsub("(b)(c)(d)", "\\1", "abcde", perl=TRUE))
-  expect_equal(str_replace_all("abcde", "(b)(c)(d)", "\\2"), gsub("(b)(c)(d)", "\\2", "abcde", perl=TRUE))
-  expect_equal(str_replace_all("abcde", "(b)(c)(d)", "\\3"), gsub("(b)(c)(d)", "\\3", "abcde", perl=TRUE))
+  expect_equal(str_replace_all("abcde", "(b)(c)(d)", "\\1"), "abe")
+  expect_equal(str_replace_all("abcde", "(b)(c)(d)", "\\2"), "ace")
+  expect_equal(str_replace_all("abcde", "(b)(c)(d)", "\\3"), "ade")
 
   # gsub("(b)(c)(d)", "\\0", "abcde", perl=TRUE) gives a0e,
   # in ICU regex $0 refers to the whole pattern match
@@ -19,11 +19,14 @@ test_that("replacement strings with capture groups refs and dollar signs work", 
 
   # gsub("(b)(c)(d)", "\\4", "abcde", perl=TRUE) is legal,
   # in ICU regex this gives an U_INDEX_OUTOFBOUNDS_ERROR
-  expect_error(str_replace_all("abcde", "(b)(c)(d)", "\\4"))
+  expect_error(
+    str_replace_all("abcde", "(b)(c)(d)", "\\4"),
+    "index that is out of bounds"
+  )
 
-  expect_equal(str_replace_all("abcde", "bcd", "\\\\1"), gsub("bcd", "\\\\1", "abcde", perl=TRUE))
+  expect_equal(str_replace_all("abcde", "bcd", "\\\\1"), "a\\1e")
 
-  expect_equal(str_replace_all("a!1!2!b", "!", "$"), gsub("!", "$", "a!1!2!b", perl=TRUE))
+  expect_equal(str_replace_all("a!1!2!b", "!", "$"), "a$1$2$b")
   expect_equal(str_replace("aba", "b", "$"), "a$a")
   expect_equal(str_replace("aba", "b", "$$$"), "a$$$a")
   expect_equal(str_replace("aba", "(b)", "\\1$\\1$\\1"), "ab$b$ba")
