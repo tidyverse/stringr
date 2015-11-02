25180cac6ca27255f960ef15d04f57a87c4e54f4
hadley <h.wickham@gmail.com>
2015-11-02 07:58:33 -0500

    Implement str_trunc.
    Fixes #65.

--- R/pad-trim.r
+++ R/pad-trim.r
@@ -7,7 +7,8 @@
 #' @param side Side on which padding character is added (left, right or both).
 #' @param pad Single padding character (default is a space).
 #' @return A character vector.
-#' @seealso \code{\link{str_trim}} to remove whitespace
+#' @seealso \code{\link{str_trim}} to remove whitespace;
+#'   \code{\link{str_trunc}} to decrease the maximum width of a string.
 #' @export
 #' @examples
 #' rbind(
@@ -32,6 +33,42 @@ str_pad <- function(string, width, side = c("left", "right", "both"), pad = " ")
     both = stri_pad_both(string, width, pad = pad)
   )
 }
+
+#' Truncate a character string.
+#'
+#' @param string A character vector.
+#' @param width Maximum width of string.
+#' @param side,ellipsis Location and content of ellipsis that indicates
+#'   content has been removed.
+#' @seealso \code{\link{str_pad}} to increase the minimum width of a string.
+#' @export
+#' @examples
+#' x <- "This string is moderately long"
+#' rbind(
+#'   str_trunc(x, 20, "right"),
+#'   str_trunc(x, 20, "left"),
+#'   str_trunc(x, 20, "center")
+#' )
+#'
+str_trunc <- function(string, width, side = c("right", "left", "center"),
+                      ellipsis = "...") {
+  side <- match.arg(side)
+
+  too_long <- str_length(string) > width
+  width... <- width - str_length(ellipsis)
+
+  string[too_long] <- switch(side,
+    right  = str_c(str_sub(string[too_long], 1, width...), ellipsis),
+    left   = str_c(ellipsis, str_sub(string[too_long], -width..., -1)),
+    center = str_c(
+        str_sub(string[too_long], 1, ceiling(width... / 2)),
+        ellipsis,
+        str_sub(string[too_long], -floor(width... / 2), -1)
+      )
+  )
+  string
+}
+
 
 #' Trim whitespace from start and end of string.
 #'
