e5073ed2ce8c098c1d474da675345ddd9f8cdecb
hadley <h.wickham@gmail.com>
2017-11-14 10:22:10 -0600

    Split up pad-trim into three files

---
+++ R/trunc.R
@@ -0,0 +1,36 @@
+#' Truncate a character string.
+#'
+#' @param string A character vector.
+#' @param width Maximum width of string.
+#' @param side,ellipsis Location and content of ellipsis that indicates
+#'   content has been removed.
+#' @seealso \code{\link{str_pad}} to increase the minimum width of a string.
+#' @export
+#' @examples
+#' x <- "This string is moderately long"
+#' rbind(
+#'   str_trunc(x, 20, "right"),
+#'   str_trunc(x, 20, "left"),
+#'   str_trunc(x, 20, "center")
+#' )
+#'
+str_trunc <- function(string, width, side = c("right", "left", "center"),
+                      ellipsis = "...") {
+  side <- match.arg(side)
+
+  too_long <- str_length(string) > width
+  too_long[is.na(too_long)] <- FALSE
+
+  width... <- width - str_length(ellipsis)
+
+  string[too_long] <- switch(side,
+    right  = str_c(str_sub(string[too_long], 1, width...), ellipsis),
+    left   = str_c(ellipsis, str_sub(string[too_long], -width..., -1)),
+    center = str_c(
+        str_sub(string[too_long], 1, ceiling(width... / 2)),
+        ellipsis,
+        str_sub(string[too_long], -floor(width... / 2), -1)
+      )
+  )
+  string
+}
