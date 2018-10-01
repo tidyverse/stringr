face911aef0d6399a20c6adf3fa92a085e20abe5
Hiroaki Yutani <yutani.ini@gmail.com>
2018-10-01 22:17:31 +0900

    Add failing tests

--- tests/testthat/test-subset.r
+++ tests/testthat/test-subset.r
@@ -6,11 +6,20 @@ test_that("basic subsetting for fixed patterns works", {
     str_subset(c("i", "I"), fixed("i", ignore_case = TRUE)),
     c("i", "I")
   )
+
+  # negation works
+  expect_equal(str_subset(c("i", "I"), fixed("i"), negate = TRUE), "I")
 })
 
 test_that("str_which is equivalent to grep", {
   expect_equal(
     str_which(head(letters), "[aeiou]"),
     grep("[aeiou]", head(letters))
+  )
+
+  # negation works
+  expect_equal(
+    str_which(head(letters), "[aeiou]", negate = TRUE),
+    grep("[aeiou]", head(letters), invert = TRUE)
   )
 })
