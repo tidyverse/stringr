48ccb5f690dccc1c504f11293ffb49e0ca05aa13
Hadley Wickham <h.wickham@gmail.com>
2021-05-01 09:27:58 -0500

    Tweak sort docs/style

--- R/sort.R
+++ R/sort.R
@@ -1,15 +1,16 @@
 #' Order or sort a character vector
 #'
+#' `str_order()` returns an integer vector that returns the desired order
+#' when used for subsetting; `str_sort()` returns the sorted vector.
+#'
 #' @param x A character vector to sort.
 #' @param decreasing A boolean. If `FALSE`, the default, sorts from
 #'   lowest to highest; if `TRUE` sorts from highest to lowest.
 #' @param na_last Where should `NA` go? `TRUE` at the end,
 #'   `FALSE` at the beginning, `NA` dropped.
-#' @param locale In which locale should the sorting occur? Defaults to
-#'   the English. This ensures that code behaves the same way across
-#'   platforms.
 #' @param numeric If `TRUE`, will sort digits numerically, instead
 #'    of as strings.
+#' @inheritParams coll
 #' @param ... Other options used to control sorting order. Passed on to
 #'   [stringi::stri_opts_collator()].
 #' @seealso [stringi::stri_order()] for the underlying implementation.
@@ -24,16 +25,34 @@
 #' x <- c("100a10", "100a5", "2b", "2a")
 #' str_sort(x)
 #' str_sort(x, numeric = TRUE)
-str_order <- function(x, decreasing = FALSE, na_last = TRUE,
-                      locale = "en", numeric = FALSE, ...) {
-  stri_order(x, decreasing = decreasing, na_last = na_last,
-    opts_collator = stri_opts_collator(locale, numeric = numeric, ...))
+str_order <- function(x,
+                      decreasing = FALSE,
+                      na_last = TRUE,
+                      locale = "en",
+                      numeric = FALSE,
+                      ...) {
+
+  opts <- stri_opts_collator(locale, numeric = numeric, ...)
+  stri_order(x,
+    decreasing = decreasing,
+    na_last = na_last,
+    opts_collator = opts
+  )
 }
 
 #' @export
 #' @rdname str_order
-str_sort <- function(x, decreasing = FALSE, na_last = TRUE,
-                     locale = "en", numeric = FALSE, ...) {
-  stri_sort(x, decreasing = decreasing, na_last = na_last,
-    opts_collator = stri_opts_collator(locale, numeric = numeric, ...))
+str_sort <- function(x,
+                     decreasing = FALSE,
+                     na_last = TRUE,
+                     locale = "en",
+                     numeric = FALSE,
+                     ...) {
+
+  opts <- stri_opts_collator(locale, numeric = numeric, ...)
+  stri_sort(x,
+    decreasing = decreasing,
+    na_last = na_last,
+    opts_collator = opts
+  )
 }
