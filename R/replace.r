58b51e9d84dc189dbc88dbef7a39fee4575a64e3
Hadley Wickham <h.wickham@gmail.com>
2022-10-04 07:58:13 -0500

    Allow empty strings in more places

--- R/replace.r
+++ R/replace.r
@@ -172,7 +172,7 @@ fix_replacement_one <- function(x) {
 #' @examples
 #' str_replace_na(c(NA, "abc", "def"))
 str_replace_na <- function(string, replacement = "NA") {
-  check_string(replacement)
+  check_string(replacement, allow_empty = TRUE)
   stri_replace_na(string, replacement)
 }
 
