de85c684706b3d37461a036b2fc555fa2ecec913
chrimaho <chrismahoney@hotmail.com>
2023-06-10 17:11:02 +1000

    Add new `str_dedent` function

--- tests/testthat/test-remove.R
+++ tests/testthat/test-remove.R
@@ -2,3 +2,17 @@ test_that("succesfully wraps str_replace_all", {
   expect_equal(str_remove_all("abababa", "ba"), "a")
   expect_equal(str_remove("abababa", "ba"), "ababa")
 })
+
+test_that("successfully dedent str_dedent",{
+  expect_equal(dedent("  Hello\n    World"), "Hello\n  World")
+  expect_equal(dedent("  Line 1\n  Line 2\n  Line 3"), "Line 1\nLine 2\nLine 3")
+  expect_equal(dedent("No indentation"), "No indentation")
+  expect_equal(dedent(
+      "
+      this
+      is
+          a
+      test
+      "
+  ), "\nthis\nis\n    a\ntest\n")
+})
