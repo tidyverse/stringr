f555ba451ed9d5fb5fc0390fa445cee82cb0a388
edward-burn <9583964+edward-burn@users.noreply.github.com>
2024-08-15 19:21:43 +0100

    add sep argument to str_dup
    closes #487
    
    Adds a sep argument to str_dup. Becasue this is not supported by stri_dup, do separately and use lapply to go through strings.

--- R/dup.R
+++ R/dup.R
@@ -5,6 +5,7 @@
 #'
 #' @inheritParams str_detect
 #' @param times Number of times to duplicate each string.
+#' @param sep String to insert between each duplicate.
 #' @return A character vector the same length as `string`/`times`.
 #' @export
 #' @examples
@@ -12,7 +13,21 @@
 #' str_dup(fruit, 2)
 #' str_dup(fruit, 1:3)
 #' str_c("ba", str_dup("na", 0:5))
-str_dup <- function(string, times) {
+str_dup <- function(string, times, sep = NULL) {
   vctrs::vec_size_common(string = string, times = times)
-  stri_dup(string, times)
+  if(is.null(sep)){
+    stri_dup(string, times)
+  } else {
+  # stri_dup does not currently support sep
+  check_string(sep)
+  lapply(seq_along(string), function(i) {
+    if (length(times) == 1) {
+      paste(rep(string[i], times), collapse = sep)
+    } else {
+      paste(rep(string[i], times[i]), collapse = sep)
+    }
+  }) |>
+    rlang::flatten_chr()
+  }
+
 }
--- tests/testthat/test-dup.R
+++ tests/testthat/test-dup.R
@@ -13,3 +13,20 @@ test_that("0 duplicates equals empty string", {
 test_that("uses tidyverse recycling rules", {
   expect_error(str_dup(1:2, 1:3), class = "vctrs_error_incompatible_size")
 })
+
+test_that("uses sep argument", {
+  expect_equal(str_dup("a", 3, sep = ";"), "a;a;a")
+  expect_equal(str_dup("abc", 2, sep = "-"), "abc-abc")
+  expect_equal(str_dup("a", 3, sep = ";"), "a;a;a")
+  expect_equal(str_dup("abc", 2, sep = "-"), "abc-abc")
+  expect_equal(str_dup(c("a", "b"), 2, sep = "x"), c("axa", "bxb"))
+  expect_equal(str_dup(c("aa", "bb", "ccc"), c(2, 3, 4), sep = ";"),
+               c("aa;aa", "bb;bb;bb", "ccc;ccc;ccc;ccc"))
+
+  expect_error(str_dup("a", 3, sep = 1))
+  expect_error(str_dup("a", 3, sep = c("-", ";")))
+  expect_error(str_dup(c("aa", "bb"), c(2, 3, 4), sep = ";"))
+  expect_error(str_dup(c("aa", "bb", "cc"), c(2, 3), sep = ";"))
+
+})
+
