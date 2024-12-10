9f8dca5d9e491c7e72b1a20c8f14d7b551aa1b5d
Vishwarrior26 <67435125+VisruthSK@users.noreply.github.com>
2024-12-10 12:51:05 -0800

    Converted stringr_view to S7.

---
+++ R/zzz.R
@@ -0,0 +1,7 @@
+.onLoad <- function(lib, pkg) {
+  S7::methods_register()
+}
+
+# enable usage of <S7_object>@name in package code
+#' @rawNamespace if (getRversion() < "4.3.0") importFrom("S7", "@")
+NULL
