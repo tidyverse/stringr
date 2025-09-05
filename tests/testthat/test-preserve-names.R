587a7da1104fcd8fc05382e29bdf575a70b59854
Jon Olav Vik <jon.vik@nmbu.no>
2025-09-05 15:35:30 +0200

    Add tests for dropping names if string is scalar and pattern/replacement isn't.

--- tests/testthat/test-preserve-names.R
+++ tests/testthat/test-preserve-names.R
@@ -87,3 +87,61 @@ test_that("str_wrap() preserves names", {
 test_that("str_split_i() preserves names", {
   expect_equal(names(str_split_i(x, " ", 1)), names(x))
 })
+
+test_that("drops names when pattern is vector but string is scalar", {
+  x1 <- c(A = "ab")
+  p2 <- c("a", "b")
+
+  # logical/integer vector outputs
+  expect_null(names(str_detect(x1, p2)))
+  expect_null(names(str_starts(x1, p2)))
+  expect_null(names(str_ends(x1, p2)))
+  expect_null(names(str_like(x1, p2)))
+  expect_null(names(str_ilike(x1, p2)))
+  expect_null(names(str_count(x1, p2)))
+
+  # character vector outputs
+  expect_null(names(str_extract(x1, p2)))
+  expect_null(names(str_replace(x1, p2, "x")))
+  expect_null(names(str_replace_all(x1, p2, "x")))
+  expect_null(names(str_split_i(x1, p2, 1)))
+
+  # list/matrix outputs: no names on outer structure/rownames when expanded
+  expect_null(names(str_extract_all(x1, p2)))
+  expect_null(names(str_split(x1, p2)))
+  expect_null(rownames(str_split(x1, p2, simplify = TRUE)))
+  expect_null(rownames(str_split_fixed(x1, p2, 1)))
+  expect_null(rownames(str_locate(x1, p2)))
+  expect_null(names(str_locate_all(x1, p2)))
+  expect_null(rownames(str_match(x1, p2)))
+  expect_null(names(str_match_all(x1, p2)))
+})
+
+test_that("preserves names when pattern and string have same length", {
+  x2 <- c(A = "ab", B = "cd")
+  p2 <- c("a", "c")
+
+  # logical/integer vector outputs
+  expect_equal(names(str_detect(x2, p2)), names(x2))
+  expect_equal(names(str_starts(x2, p2)), names(x2))
+  expect_equal(names(str_ends(x2, p2)), names(x2))
+  expect_equal(names(str_like(x2, p2)), names(x2))
+  expect_equal(names(str_ilike(x2, p2)), names(x2))
+  expect_equal(names(str_count(x2, p2)), names(x2))
+
+  # character vector outputs
+  expect_equal(names(str_extract(x2, p2)), names(x2))
+  expect_equal(names(str_replace(x2, p2, "x")), names(x2))
+  expect_equal(names(str_replace_all(x2, p2, "x")), names(x2))
+  expect_equal(names(str_split_i(x2, p2, 1)), names(x2))
+
+  # list/matrix outputs: preserve names/rownames at 1:1 size
+  expect_equal(names(str_extract_all(x2, p2)), names(x2))
+  expect_equal(names(str_split(x2, p2)), names(x2))
+  expect_equal(rownames(str_split(x2, p2, simplify = TRUE)), names(x2))
+  expect_equal(rownames(str_split_fixed(x2, p2, 1)), names(x2))
+  expect_equal(rownames(str_locate(x2, p2)), names(x2))
+  expect_equal(names(str_locate_all(x2, p2)), names(x2))
+  expect_equal(rownames(str_match(x2, p2)), names(x2))
+  expect_equal(names(str_match_all(x2, p2)), names(x2))
+})
