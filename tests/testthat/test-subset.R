tidyverse/stringr

commit d681e457000cde9e504ee9bea0ce10f54687f811

str_subset to keep names if present (#560)
closes #507

R/subset.R
@@ -31,12 +31,12 @@ str_subset <- function(string, pattern, negate = FALSE) {
   check_bool(negate)
 
   switch(type(pattern),
-    empty = no_empty(),
-    bound = no_boundary(),
-    fixed = stri_subset_fixed(string, pattern, omit_na = TRUE, negate = negate, opts_fixed = opts(pattern)),
-    coll  = stri_subset_coll(string, pattern, omit_na = TRUE, negate = negate, opts_collator = opts(pattern)),
-    regex = stri_subset_regex(string, pattern, omit_na = TRUE, negate = negate, opts_regex = opts(pattern))
-  )
+         empty = no_empty(),
+         bound = no_boundary(),
+         fixed = string[str_detect(string, pattern, negate = negate)],
+         coll  = string[str_detect(string, pattern, negate = negate)],
+         regex = string[str_detect(string, pattern, negate = negate)])
+
 }
 
 #' Find matching indices

tests/testthat/test-subset.R
@@ -39,3 +39,10 @@ test_that("can't use boundaries", {
     str_subset(c("a", "b c"), boundary())
   })
 })
+
+test_that("keep names", {
+  fruit <- c(A = "apple", B = "banana", C = "pear", D = "pineapple")
+  expect_identical(names(str_subset(fruit, "b")), "B")
+  expect_identical(names(str_subset(fruit, "p")), c("A", "C", "D"))
+  expect_identical(names(str_subset(fruit, "x")), as.character())
+})

commit ec5172742aeb1b34dd2d41a6bf0f93c736556b80

Have str_subset() drop NAs from string.
The "missings never match" example from ?str_subset broke in commit d681e45, which added name preservation.
The current commit keeps names while not matching NAs in string.
Also added a test for missings never to match.

> # stringr 1.5.2
> stringr::str_subset(c("a", NA, "b"), ".")
[1] "a" "b"

> # pak::pkg_install("tidyverse/stringr@d681e45")
> stringr::str_subset(c("a", NA, "b"), ".")
[1] "a" NA  "b"
> stringr::str_subset(c(First = "a", Second = NA, Third = "b"), ".")
First  <NA> Third
  "a"    NA   "b"

> # Current version
> stringr::str_subset(c(First = "a", Second = NA, Third = "b"), ".")
First Third
  "a"   "b"

R/subset.R
@@ -30,13 +30,18 @@ str_subset <- function(string, pattern, negate = FALSE) {
   check_lengths(string, pattern)
   check_bool(negate)
 
-  switch(type(pattern),
-         empty = no_empty(),
-         bound = no_boundary(),
-         fixed = string[str_detect(string, pattern, negate = negate)],
-         coll  = string[str_detect(string, pattern, negate = negate)],
-         regex = string[str_detect(string, pattern, negate = negate)])
+  if (type(pattern) == "empty") no_empty()
+  if (type(pattern) == "bound") no_boundary()
 
+  idx <- str_detect(string, pattern, negate = negate)
+  # str_detect() returns NA for NAs in string, but str_subset() should drop them
+  idx[is.na(idx)] <- FALSE
+  out <- string[idx]
+  # Work around the fact that as.character() drops names
+  nm <- names(out)
+  out <- as.character(out)
+  names(out) <- nm
+  out
 }
 
 #' Find matching indices

tests/testthat/test-subset.R
@@ -52,3 +52,8 @@ test_that("str_subset() preserves names of retained elements", {
   out <- str_subset(x, "[12]")
   expect_equal(names(out), c("B", "A"))
 })
+
+test_that("str_subset() never matches missing values", {
+  expect_equal(str_subset(c("a", NA, "b"), "."), c("a", "b"))
+  expect_identical(str_subset(NA, "."), character(0))
+})